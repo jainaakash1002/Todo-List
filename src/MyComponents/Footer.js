2054286115843cd65bea94540d28ba2153cde492
Aakash Jain <jainaakash1002@gmail.com>
2023-04-21 22:04:34 +0530

    initial commit

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer className="bg-dark text-light py-3">
+      <p className="text-center">&copy;All rights reserved.</p>
+    </footer>
+  );
+};
